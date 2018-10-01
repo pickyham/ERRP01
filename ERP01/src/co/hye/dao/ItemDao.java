83280c5536022e90fe533e4ae2c03f8ac2b31be9
pickyham <ringkle777@gmail.com>
2018-10-01 17:15:06 +0900

    itemdao05:15

--- ERP01/src/co/hye/dao/ItemDao.java
+++ ERP01/src/co/hye/dao/ItemDao.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
+
+import co.hye.bean.ItemBean;
 
 public class ItemDao {
 	private Connection conn;
@@ -31,11 +34,32 @@ public class ItemDao {
 				+ "values(?, ?, ?, ?, ?)";
 		try {
 			psmt = conn.prepareStatement(sql);
-			psmt.setInt(1, i.getiClass());
+			psmt.setString(1, i.getiClass());
 			int n = psmt.executeUpdate();
 
 			if (n == 0) System.out.println("품목 정보 등록 실패");
 			else System.out.println("품목 정보가 등록되었습니다.");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void DeleteItem(ItemBean i) {
+		String sql = "delete from item_t where icode = ?";
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("삭제할 코드를 입력 하세요.");
+		System.out.println("A-필기구/B-용지/C-");
+		String n = sc.nextLine();
+		sc.close();
+		
+		try {			
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, n);
+			int r = psmt.executeUpdate();
+			
+			if (r == 0) System.out.println("삭제 실패");
+			else System.out.println("삭제 성공");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
