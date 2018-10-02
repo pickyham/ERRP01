23b2c73d16440fe3a74d6a0b582288d30f02ee9f
pickyham <ringkle777@gmail.com>
2018-10-02 12:41:41 +0900

    i,idao

--- ERP01/src/co/hye/dao/ItemDao.java
+++ ERP01/src/co/hye/dao/ItemDao.java
@@ -54,7 +54,7 @@ public class ItemDao {
 	
 	//작업필요
 	//파라메터 : ItemBean i
-	public void DeleteItem() {
+	public void DeleteItem(ItemBean i) {
 		String sql = "delete from item_t where icode = ?";
 		
 		Scanner sc = new Scanner(System.in);
@@ -75,10 +75,52 @@ public class ItemDao {
 		}
 	}
 	
-	public void UpdateItem() {
-		String sql = "";
+	public void UpdateItem(int c, ItemBean i) throws SQLException, ClassNotFoundException {
+		String qeury;			
+		//m = Item.iupdate(null);
 		
-
+		String sql = "update item_t set $col = ? where icode=?";		
+		try {
+			psmt.setString(2, i.getiName());
+			switch(c) {
+			case 1:
+				qeury = sql.replace("$col", "iclass");
+				psmt.setString(1, i.getiClass());
+				psmt = conn.prepareStatement(qeury);
+				break;	
+			case 2:
+				qeury = sql.replace("$col", "icode");
+				psmt.setString(1, i.getiCode());
+				psmt = conn.prepareStatement(qeury);
+				break;	
+			case 3:
+				qeury = sql.replace("$col", "iname");
+				psmt.setString(1, i.getiName());
+				psmt = conn.prepareStatement(qeury);
+				break;	
+			case 4:
+				qeury = sql.replace("$col", "istandard");
+				psmt.setString(1, i.getiStandard());
+				psmt = conn.prepareStatement(qeury);
+				break;	
+			case 5:
+				qeury = sql.replace("$col", "iunit");
+				psmt.setString(1, i.getiUnit());
+				psmt = conn.prepareStatement(qeury);
+				break;	
+			case 6:
+				qeury = sql.replace("$col", "cname");
+				psmt.setString(1, i.getcName());
+				psmt = conn.prepareStatement(qeury);
+				break;
+			}
+			psmt = conn.prepareStatement(sql);
+			int n = psmt.executeUpdate();
+			if (n ==0) System.out.println("변경 실패");
+			else System.out.println("변경 성공");			
+		} catch (SQLException e ) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void SelectItem() {
