32dc1e41b81d4a4756478d17b0b7541475f8930e
User <User@YD03-22>
2018-10-10 17:26:29 +0900

    Inout

---
+++ ERP01/src/co/hye/dao/InoutDao.java
@@ -0,0 +1,43 @@
+package co.hye.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class InoutDao {
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+	private String sql;
+
+	public InoutDao() throws ClassNotFoundException {
+		try {
+			String user = "ham";
+			String pw = "ham";
+			String url = "jdbc:oracle:thin:@192.168.0.95:1521:xe";
+	
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, pw);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ResultSet SelectInout() {
+		sql = "select io.ioseq, io.icode, i.iname, io.pea, io.sea, io.ea, io.hname "
+			+ "from inout_t io join item_t i "
+			+ "on io.icode = i.icode "
+			+ "order by ioseq";
+		try {
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+}
